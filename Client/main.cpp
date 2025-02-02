7218dc3f6ce5e7c11c171bbc2fbad92a5048fe57
nwiart <nwiart@gaming.tech>
2025-02-02 17:45:51 +0100

    SFML dummy window

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,5 +1,22 @@
+#include <SFML/Graphics.hpp>
+
 
 int main(int argc, const char** argv)
 {
+	// Window without resize.
+	sf::Window window;
+	window.create(sf::VideoMode(1280, 720), "Pong Multiplayer", sf::Style::Default & ~sf::Style::Resize);
+	window.setVerticalSyncEnabled(true);
+
+	bool quit = false;
+	while (!quit) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				quit = true;
+			}
+		}
+	}
+
 	return 0;
 }
