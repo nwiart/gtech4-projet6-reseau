193276e6ffd1bab3cea9df1d99608b29ef0c10cd
Gotvna <92024828+Gotvna@users.noreply.github.com>
2025-02-04 17:37:56 +0100

    ball serveur

--- Server/Pong/Ball.h
+++ Server/Pong/Ball.h
@@ -1,19 +1,25 @@
 #ifndef BALL_H
 #define BALL_H
 
-#include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
 
 class PongScene;
 
 class Ball {
 public:
     Ball();
-    void update(float dt, PongScene* scene, sf::RectangleShape& paddle1, sf::RectangleShape& paddle2);
+
+    void update(float dt, PongScene* scene);
     void reset(float startX, float startY);
-    const sf::Vector2f& getPosition() const;
+
+    const sf::Vector2f& getPosition() const { return position; }
+    const sf::Vector2f& getVelocity() const { return velocity; }
+
+    void reverseX();
+    void reverseY();
 
 private:
-    sf::CircleShape shape;
+    sf::Vector2f position;
     sf::Vector2f velocity;
 };
 
