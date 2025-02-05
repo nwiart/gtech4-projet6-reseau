21c0afc6207691266f12f24b0e1f190ae4dfcae3
Gotvna <92024828+Gotvna@users.noreply.github.com>
2025-02-05 10:11:01 +0100

    ball only display shape and position client side

--- Client/Ball.cpp
+++ Client/Ball.cpp
@@ -1,50 +1,20 @@
 #include "Ball.h"
 
 Ball::Ball(float startX, float startY) {
-    shape.setRadius(10);
+    shape.setRadius(10.f);
     shape.setFillColor(sf::Color::White);
+    shape.setOrigin(10.f, 10.f);
     shape.setPosition(startX, startY);
-    velocity = sf::Vector2f(-5, 5);
 }
 
-void Ball::update(sf::RenderWindow& window, sf::RectangleShape& paddle1, sf::RectangleShape& paddle2) {
-    shape.move(velocity);
-
-    if (shape.getPosition().y <= 0 || shape.getPosition().y + shape.getRadius() * 2 >= window.getSize().y) {
-        velocity.y *= -1;
-    }
-
-    if (shape.getGlobalBounds().intersects(paddle1.getGlobalBounds()) ||
-        shape.getGlobalBounds().intersects(paddle2.getGlobalBounds())) {
-        velocity.x *= -1;
-    }
-}
-
-void Ball::draw(sf::RenderWindow& window) {
-    window.draw(shape);
-}
-
-void Ball::reset(float startX, float startY) {
-    shape.setPosition(startX, startY);
-    velocity = sf::Vector2f((rand() % 2 == 0 ? -5 : 5), (rand() % 2 == 0 ? -5 : 5)); // Direction aléatoire
+void Ball::setPosition(const sf::Vector2f& pos) {
+    shape.setPosition(pos);
 }
 
 sf::Vector2f Ball::getPosition() const {
     return shape.getPosition();
 }
 
-bool Ball::isOutOfBounds(sf::Vector2u windowSize, bool& scoredForPlayer1, bool& scoredForPlayer2) {
-    float ballRight = shape.getPosition().x + shape.getRadius() * 2;
-    float ballLeft = shape.getPosition().x;
-
-    if (ballRight < 0) {
-        scoredForPlayer2 = true;
-        return true;
-    }
-    else if (ballLeft > windowSize.x) {
-        scoredForPlayer1 = true;
-        return true;
-    }
-
-    return false;
+void Ball::draw(sf::RenderWindow& window) {
+    window.draw(shape);
 }
--- Client/Ball.h
+++ Client/Ball.h
@@ -6,15 +6,13 @@
 class Ball {
 public:
     Ball(float startX, float startY);
-    void update(sf::RenderWindow& window, sf::RectangleShape& paddle1, sf::RectangleShape& paddle2);
+
     void draw(sf::RenderWindow& window);
-    void reset(float startX, float startY);
+    void setPosition(const sf::Vector2f& pos);
     sf::Vector2f getPosition() const;
-    bool isOutOfBounds(sf::Vector2u windowSize, bool& scoredForPlayer1, bool& scoredForPlayer2);
 
 private:
     sf::CircleShape shape;
-    sf::Vector2f velocity;
 };
 
 #endif
